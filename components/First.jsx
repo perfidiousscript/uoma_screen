4d70e48038f35f3f988f451df0e505c2238ec5a9
Sam Moss <sam.moss321@gmail.com>
2020-09-07 11:32:57 -0700

    allows user to select choice

--- components/First.jsx
+++ components/First.jsx
@@ -1,3 +1,44 @@
+import React, { useEffect, useState } from "react";
+
 export default function First() {
-  return <p>First page here</p>;
+  const [selectedChoice, setChoice] = useState(null);
+
+  function selectedBackground(e) {
+    setChoice(e.target.dataset.choice);
+    e.target.class = "chartreuse";
+    e.target.style.color = "black";
+  }
+
+  function unselectedBackground(e) {
+    setChoice(null);
+    e.target.style.background = "black";
+    e.target.style.color = "chartreuse";
+  }
+
+  useEffect(() => {});
+
+  return (
+    <>
+      <p className="lastLine"> </p>
+      <p className="currentLine"> </p>
+      <div className="choices">
+        <span
+          onMouseEnter={selectedBackground}
+          onMouseLeave={unselectedBackground}
+          className="choice"
+          data-choice="c1"
+        >
+          Choice 1
+        </span>
+        <span
+          onMouseEnter={selectedBackground}
+          onMouseLeave={unselectedBackground}
+          className="choice"
+          data-choice="c2"
+        >
+          Choice 2
+        </span>
+      </div>
+    </>
+  );
 }
