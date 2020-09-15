97782a255f493d4951ff19cf36e74e08e4586519
Sam Moss <sam.moss321@gmail.com>
2020-09-15 15:10:27 -0700

    adds basic story material file

---
+++ story_material/story.js
@@ -0,0 +1,19 @@
+export const story_json = {
+  "0": {
+    text: [
+      ["An Echo"],
+      ["That’s what it is: an echo."],
+      ["I’ll remember what I was, at some point."],
+      ["It will come back to me."]
+    ],
+    choices: [{ text: "Choice 1", id: "1" }, { text: "Choice 2", id: "1" }]
+  },
+  "1": {
+    text: [["This is the new choice"], ["It works really well"]],
+    choices: [
+      { text: "These are some new choices", id: "0" },
+      { text: "They work well", id: "0" },
+      { text: "And look good", id: "0" }
+    ]
+  }
+};
