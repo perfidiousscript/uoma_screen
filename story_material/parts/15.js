1c7722f7a6f419865d21511bd745540b710e8820
Sam Moss <sam.moss321@gmail.com>
2020-12-01 13:32:18 -0800

    adds part 15

---
+++ story_material/parts/15.js
@@ -0,0 +1,70 @@
+export const fifteen = {
+  "0": {
+    a: {
+      text: [
+        ["Sep came up behind the man. He was holding a length of cattle rope."],
+        [" He casually threw a looped around the man`s neck and pulled, hard."],
+        [
+          "Any other person would have tumbled down the stairs, but the man barely budged. He didn`t even turn around, just kept pounding at the door of the shrine harder and harder."
+        ],
+        ['Sep turned to me, "Grab hold, grab hold!"'],
+        [
+          "I grabbed onto the rope and pulled. Even with the two of us pulling the resistance was incredible."
+        ],
+        [
+          "He threw his shoulder against the door and pulled us along with him."
+        ],
+        [
+          "James and another woman came up the stairs and grabbed hold of the rope. With the four of us together we were able to pull the man away from the door and down a few stairs."
+        ],
+        ['"Down the Path!" Sep called.'],
+        [
+          "We continued to pull. After twenty feet or so I was winded. Yet the man continued to walk toward the shrine digging into the dust with a fantastic strength."
+        ],
+        [
+          "The rope was only so long, and only two others could help pull. It seemed to take forever to reach the edge of Uoma."
+        ],
+        [
+          'As we came outside the edge of the settlement Sep said "After we go a little further I want you all to hold him, alright?"'
+        ],
+        ['We all said "Yes" between pants.'],
+        [
+          "How had he not choked? How had he not been bowled over by the rope? It was like pulling an animal, or a machine."
+        ],
+        ['"Hold tight!" Sep said.'],
+        [
+          "The three of us dug into the dust and held on to the man as Sep let go of the rope."
+        ],
+        ["There was a jolt but we held fast."],
+        ["Sep pulled a revolver out of his jacket."],
+        [
+          "I tried to call out but there was only enough time to look away before there was a crack and we all fell to the ground."
+        ],
+        [
+          "I stood shaking. James and the woman stood calmly, dusting themselves off."
+        ],
+        ['"Jesus Christ Sep!"'],
+        ["I looked at the body, or where it should have been."],
+        [
+          "But there was nothing there besides that dirty woolen cloak in a heap on the ground."
+        ],
+        ["I looked around but he was nowhere to be seen."],
+        ["Sep turned the cloak over with the toe of his boot."],
+        ["James and the woman came and stood over it without saying anything."],
+        ['"Burn this would you?" Sep said quietly.'],
+        ["The woman nodded. She and James walked off back into Uoma."],
+        ["Sep checked the gun then holstered it under his jacket."],
+        ["He turned toward me and took a deep breath in through his nose."]
+      ],
+      choices: [
+        {
+          text: [
+            '"It`s time for evening meal." Sep said, turning. "Let`s go get something to eat."'
+          ],
+          id: { part: "18", sub: "a" }
+        }
+      ]
+    }
+  },
+  "1": {}
+};
--- story_material/story.js
+++ story_material/story.js
@@ -13,6 +13,7 @@ import { eleven } from "./parts/11.js";
 import { twelve } from "./parts/12.js";
 import { thirteen } from "/.parts/13.js";
 import { fourteen } from "./parts/14.js";
+import { fifteen } from "./parts/15.js";
 import { twenty } from "./parts/20.js";
 import { wander_1 } from "./wander/wander_1.js";
 import { wander_2, found } from "./wander/wander_2.js";
@@ -32,6 +33,7 @@ export const story_json = {
   "12": twelve,
   "13": thirteen,
   "14": fourteen,
+  "15": fifteen,
   "20": twenty,
   wander_1: wander_1,
   wander_2: wander_2,
