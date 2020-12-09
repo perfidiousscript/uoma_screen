a20d3092aa41f23255d68675977d49646f7bb1ec
Sam Moss <sam.moss321@gmail.com>
2020-12-09 14:19:49 -0800

    starts work in the fade out for endings

--- story_material/story.js
+++ story_material/story.js
@@ -11,7 +11,7 @@ import { nine } from "./parts/9.js";
 import { ten } from "./parts/10.js";
 import { eleven } from "./parts/11.js";
 import { twelve } from "./parts/12.js";
-import { thirteen } from "/.parts/13.js";
+import { thirteen } from "./parts/13.js";
 import { fourteen } from "./parts/14.js";
 import { fifteen } from "./parts/15.js";
 import { sixteen } from "./parts/16.js";
@@ -21,6 +21,7 @@ import { nineteen } from "./parts/19.js";
 import { twenty } from "./parts/20.js";
 import { wander_1 } from "./wander/wander_1.js";
 import { wander_2, found } from "./wander/wander_2.js";
+import { ending } from "./parts/ending.js";
 export const story_json = {
   "0": zero,
   "1": one,
@@ -45,5 +46,6 @@ export const story_json = {
   "20": twenty,
   wander_1: wander_1,
   wander_2: wander_2,
-  found: found
+  found: found,
+  ending: ending
 };
