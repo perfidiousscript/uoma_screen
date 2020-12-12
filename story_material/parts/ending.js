b4d6768a9073d0306ccebca78dc2c463739605f8
Sam Moss <sam.moss321@gmail.com>
2020-12-11 16:30:15 -0800

    clean up ending a

--- story_material/parts/ending.js
+++ story_material/parts/ending.js
@@ -3,29 +3,35 @@ export const ending = {
     a: {
       text: [
         ["I stood fast in front of the door to the shrine."],
-        ["His hands came down as I brought my arm up to stop him."],
+        ["As a reflex I brought my arm up to stop him and he grabbed me."],
+        ["Those hands gripped down like a metal vice, senseless, unforgiving."],
+        ["When he touched me it was as if he could saw me for the first time."],
         [
-          "Those hands gripped onto my arm like a metal vice, senseless, unforgiving."
-        ],
-        ["When he touched me it was like he could see me for the first time."],
-        [
-          "Those blank eyes landed on me and suddenly filled with intelligence, but not a human intelligence."
+          "That blank gaze fell onto my own and filled with intelligence, but not any human intelligence."
         ],
         [
-          "He brought his face close to me and I could tell what he was saying, clearly, for the first time:"
+          "He brought his face close to my own and I could hear what he was saying, clearly, for the first time:"
         ],
-        ['"Donas. Donas. Donas."'],
+        ['"DONAS."'],
+        ['"DONAS."'],
+        ['"DONAS."'],
         [
           "Everything around wavered and broke, the sun turned a reddish hue, the sky blackened."
         ],
         [
-          "Time grew very slow, and I found that I could see the world as it was, as it always had been."
+          "Time grew very slow, and I found that I could see the world as it was. No...as it always had been."
         ],
         [
-          "The faces of those around me were pale and shining, clothes draped over their bodies strangely, with an intelligence that seemed strange."
+          "The faces of those watching me were pale and shone with absence. Their clothes draped over their bodies like rags."
         ],
         [
-          "There was a point far off, far off in the sky that grew close as it grew far away, I could not stop gazing up at it and it seemed to come to me as I came to it."
+          "There was a point far off, far off in the sky that at once grew close as it grew far away."
+        ],
+        [
+          "It had always been there, but I had always kept my gaze away from it."
+        ],
+        [
+          "But now I could not stop gazing up at it and it seemed to come to me as I came to it."
         ],
         ["My body..."],
         ["My mind..."],
