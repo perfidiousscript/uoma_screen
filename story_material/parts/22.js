f0e92d48eb04df10395d1c8079f39dc0ebf43c03
Sam Moss <sam.moss321@gmail.com>
2020-12-22 13:57:32 -0800

    finishes up part 22

--- story_material/parts/22.js
+++ story_material/parts/22.js
@@ -17,6 +17,28 @@ const bChoices = {
   }
 };
 
+const sepArrayOne = [
+  ["I walked over to Sep's place."],
+  ["I passed a group of Uomans on the way."]
+];
+
+const sepArrayTwo = [
+  ["When I knocked on Sep's door a young woman I had never seen answered."],
+  ["She was short, with a mousey look and younger than me."],
+  ["'Can I help you?'"],
+  ["She seemed dour, disinterested in me or my presence."],
+  ["'I'm here to see Sep.'"],
+  ["About what?"],
+  [
+    "'I'm working on a project for him. I`d like to talk to him directly if I could.'"
+  ],
+  ["She eyed me warily."],
+  ["'He's sort of busy right now.'"],
+  ["'It'll be quick. Can you just let him know that I'm here?'"],
+  ["She turned away in a huff without inviting me in."],
+  ["I heard her talking in another room and a few moments later Sep appeared."]
+];
+
 const secondChoices = {};
 //(Courage over two, Trust over two)"What is an Ash Burner, Sep."
 //(Courage over two, Trust under two)"What is an Ash Burner, Sep."
@@ -56,8 +78,10 @@ export const twentyTwo = {
         [
           "Sep's notes and drawings lay out around the studio. A hideous jumble."
         ],
-        ["I had made a few studies of the drawings in clay, set them aside."],
-        ["They had already dried out begun to crack at the surface."],
+        [
+          "I had made a few studies of the drawings in clay over the last week but had set them aside."
+        ],
+        ["They had already dried out, begun to crack at the surface."],
         ["Their eyes looked out at me, misshapen and glaring."],
         ["I suddenly felt a deep sense of dread at their presence."],
         [
@@ -67,12 +91,32 @@ export const twentyTwo = {
       choices: bChoices
     },
     c: {
-      text: [],
-      choices: []
+      text: sepArrayOne
+        .concat([
+          ["They stopped talking when I passed and watched me."],
+          [
+            "Before I might have smiled and waved, but their looks were sour and I just continued on."
+          ]
+        ])
+        .concat(sepArrayTwo),
+      choices: [
+        { text: ["He waved me in silently"], id: { part: "23", id: "h" } }
+      ]
     },
     d: {
-      text: [],
-      choices: []
+      text: sepArrayOne
+        .concat([
+          [
+            "They got quiet as I passed, one of them nodded and waved. I waved back"
+          ],
+          [
+            "I could tell there was some tension between us, but as long as Sep was on my side I figured things would smooth over."
+          ]
+        ])
+        .concat(sepArrayTwo),
+      choices: [
+        { text: ["Sep smiled and waved me in."], id: { part: "23", id: "i" } }
+      ]
     }
   }
 };
